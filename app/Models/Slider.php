894f33f1a87db6888993f846827eeba14380f0bf
Draconik513 <abenk223jr@gmail.com>
2026-08-10 12:12:26 +0800

    pdate keselrhan

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    protected $table = 'slider';
+    protected $table = 'sliders';
     
     protected $fillable = [
         'gambar', 'judul', 'urutan', 'status'
